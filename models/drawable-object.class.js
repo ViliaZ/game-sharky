7dc8887452667e438c173c14fb06e89de2302eeb
Vilia <vzeisig@gmail.com>
2022-01-21 07:42:14 +0100

    order structure for new classes finished

---
+++ models/drawable-object.class.js
@@ -0,0 +1,30 @@
+// this class contains MoveableObject
+
+class Drawableobject {
+    x = 0;
+    y = 50;
+    width = 200;
+    height = 150;
+    img;                                // if object is animated, this img is changed consistantly to create the animation
+    imageCache = [];                    //array as storage of images animation
+    currentImage = 0;                   // start animation here
+
+    loadImage(path) {
+        this.img = new Image();         // new Image() is already a JS method!  (its the same as creating <img src="">)
+        this.img.src = path;
+    }
+
+    // Filling up imageCache with all images for animation
+    loadImages(array) {
+        array.forEach((path) => {       //path is the parameter and referring to each element of the array
+            let img = new Image();      // JS method for creating an HTML object <img>
+            img.src = path;
+            this.imageCache[path] = img;// push all images into imagesCache Array --> path is the key and img is the value
+        });
+    }
+
+    // draw on context
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);  // drawImage() is a API Browser Function: ctx.drawImage(image, dx, dy, dWidth, dHeight)
+    }
+}
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,11 +1,6 @@
-class MoveableObject {
-    x = 0;
-    y = 50;
-    width = 200;
-    height = 150;
-    img;                                // if object is animated, this img is changed consistantly to create the animation
-    imageCache = [];                    //array as storage of images animation
-    currentImage = 0;                   // start animation here
+class MoveableObject extends Drawableobject{
+
+             
     speed = 0.20 + Math.random() * 0.5;   // pixels to move
     otherDirection = false;             // mirroring object e.g. character looking to left
     speedY = 0;
@@ -49,23 +44,6 @@ class MoveableObject {
     }
 
 
-    loadImage(path) {
-        this.img = new Image();         // new Image() is already a JS method!  (its the same as creating <img src="">)
-        this.img.src = path;
-    }
-    // Filling up imageCache with all images for animation
-    loadImages(array) {
-        array.forEach((path) => {       //path is the parameter and referring to each element of the array
-            let img = new Image();      // JS method for creating an HTML object <img>
-            img.src = path;
-            this.imageCache[path] = img;// push all images into imagesCache Array --> path is the key and img is the value
-        });
-    }
-
-    // draw on context
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);  // drawImage() is a API Browser Function: ctx.drawImage(image, dx, dy, dWidth, dHeight)
-    }
 
     // Green rectangle around each object >> helping programming collisions
     drawFrames(ctx) {
